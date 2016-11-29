38129baf45e19e38977714c5de5dc70f7ae33397
deakgeek <melia.deakin@gmail.com>
2016-11-29 17:39:16 -0600

    Updated querying tutorial with words instead of letters in examples

--- content/efcore/getting-started/samples/filtering-entities/Program.cs
+++ content/efcore/getting-started/samples/filtering-entities/Program.cs
@@ -9,7 +9,7 @@ public class Program
         using (var context = new LibraryContext())
         {
             var books = context.Books
-                .Where(s => s.Genre.Contains("Science Fiction"))
+                .Where(book => book.Genre == "Science Fiction")
                 .ToList();
 
             foreach (Book book in books)
