282c1cd3689bb0e1035180b64f68b7f62ba5e561
Ryan Helmoski <ryanhelmoski@gmail.com>
2017-04-24 16:26:39 -0500

    Adds missing semicolon

--- content/efcore/getting-started/samples/loading-related-data/include-and-then-include-method-chain/Program.cs
+++ content/efcore/getting-started/samples/loading-related-data/include-and-then-include-method-chain/Program.cs
@@ -16,7 +16,7 @@ public class Program
             Console.WriteLine("Book: {0}", book.Title);
             Console.WriteLine("Author: {0} {1}", book.Author.FirstName, book.Author.LastName);
 
-            Console.WriteLine("Checkout Records:")
+            Console.WriteLine("Checkout Records:");
             foreach (CheckoutRecord checkoutRecord in book.CheckoutRecords)
             {
                 Console.WriteLine("Checked out by {0} {1} on {2}", checkoutRecord.Reader.FirstName, checkoutRecord.Reader.LastName, checkoutRecord.CheckoutDate.ToString("MMMM dd, yyyy"));
