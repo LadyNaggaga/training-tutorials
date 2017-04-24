c2266ba280adb894c62e7d445b66a2e312cdddc4
Ryan Helmoski <ryanhelmoski@gmail.com>
2017-04-24 15:43:01 -0500

    Removes unnecessary fields from output for include example.

--- content/efcore/getting-started/samples/loading-related-data/include-method/Program.cs
+++ content/efcore/getting-started/samples/loading-related-data/include-method/Program.cs
@@ -12,11 +12,7 @@ public class Program
                 .Include(b => b.Author)
                 .Single(b => b.Id == 1);
 
-            Console.WriteLine("Included author with book -");
-            Console.WriteLine("Id: {0}", book.Id);
             Console.WriteLine("Title: {0}", book.Title);
-            Console.WriteLine("Genre: {0}", book.Genre);
-            Console.WriteLine("Publication Year: {0}", book.PublicationYear);
             Console.WriteLine("Author: {0} {1}", book.Author.FirstName, book.Author.LastName);
         }
     }
